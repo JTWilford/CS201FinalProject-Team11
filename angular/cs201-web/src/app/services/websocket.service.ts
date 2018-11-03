ac9ed7c4f3e69080999550b36795cd71f3efeda9
Justin Wilford <jwilford@usc.edu>
2018-11-03 14:19:33 -0700

    Added websockets to both front end and backend

---
+++ angular/cs201-web/src/app/services/websocket.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {Observable, Observer, Subject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+
+  constructor() { }
+
+  private subject: Subject<MessageEvent>;
+
+  public connect(url): Subject<MessageEvent> {
+    if(!this.subject) {
+      this.subject = this.create(url);
+      console.log("Successfully Connected: " + url);
+    }
+    return this.subject;
+  }
+
+  private create(url): Subject<MessageEvent> {
+    let ws = new WebSocket(url);
+
+    let observable = Observable.create(
+      (obs: Observer<MessageEvent>) => {
+        ws.onmessage = obs.next.bind(obs);
+        ws.onerror = obs.error.bind(obs);
+        ws.onclose = obs.complete.bind(obs);
+        return ws.close.bind(obs);
+      }
+    )
+
+    let observer = {
+      next: (data: string) => {
+        if(ws.readyState === WebSocket.OPEN) {
+          //ws.send(JSON.stringify(data));
+          ws.send(data);
+        }
+      }
+    }
+
+    return Subject.create(observer, observable);
+  }
+}
