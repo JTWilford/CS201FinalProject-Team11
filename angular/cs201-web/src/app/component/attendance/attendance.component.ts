9313fe938e96f05acdb7e3dd772c9bd224bc7e80
Justin Wilford <jwilford@usc.edu>
2018-11-08 18:51:04 -0800

    Added Geolocation API to frontend; Added comments to Attendance servlet to detail API

--- angular/cs201-web/src/app/component/attendance/attendance.component.ts
+++ angular/cs201-web/src/app/component/attendance/attendance.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AttendanceComponent implements OnInit {
 
+  private hasGeolocation = false;
+
   constructor() { }
 
   ngOnInit() {
+    if("geolocation" in navigator) {
+      console.log("User has geolocation enabled!");
+      this.hasGeolocation = true;
+    }
+    else {
+      console.log("User doesn't have geolocation enabled");
+      this.hasGeolocation = false;
+    }
+  }
+
+  getLocation() {
+    if (this.hasGeolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log("latitude: " + position.coords.latitude);
+        console.log("longitude: " + position.coords.longitude);
+      });
+    }
+    else {
+      console.log("Cannot get location");
+    }
   }
 
 }
