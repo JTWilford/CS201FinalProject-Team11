6a0add955bc45fbb8da8bb0250006e48cc93d343
Addison Herr <aherr@usc.edu>
2018-11-17 17:28:26 -0800

    Added front-end for SQL table population for lectures, labs, and exams

--- angular/cs201-web/src/app/component/previous-exams/previous-exams.component.ts
+++ angular/cs201-web/src/app/component/previous-exams/previous-exams.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Exam } from "../../interfaces/exam.interface";
+import {DataService} from "../../services/data.service";
 
 @Component({
   selector: 'app-previous-exams',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviousExamsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
+
+  hasLoaded = false;
+  exams: Exam[];
 
   ngOnInit() {
+    this.hasLoaded = false;
+    this.dataService.getExams().then((response) => {
+      console.log("[AssignmentComponent] Received Data");
+      this.exams = response;
+      console.log(this.exams);
+      this.hasLoaded = true;
+    })
   }
 
 }
