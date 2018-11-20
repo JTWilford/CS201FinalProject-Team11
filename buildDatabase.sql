ae918d4e82b45a64b39d2b694d946e33334311bf
ntsliyang <yli546@usc.edu>
2018-11-19 19:25:00 -0800

    update and add sql scripts

--- buildDatabase.sql
+++ buildDatabase.sql
@@ -45,7 +45,7 @@ CREATE TABLE Assignments (
 
 CREATE TABLE Lectures (
   lectureID INT(11) PRIMARY KEY AUTO_INCREMENT, # reference lecture table entries
-  number INT(6) UNIQUE NOT NULL, # lecture number
+  number INT(6) NOT NULL, # lecture number
   lectureDate VARCHAR(12) NOT NULL, # (MM/DD/YYYY) date of the lecture
   lectureTopics VARCHAR(150) NOT NULL, # list of topics covered in the lecture
   chapters VARCHAR(15) NOT NULL, # chapters covered in the lecture
@@ -56,21 +56,22 @@ CREATE TABLE Lectures (
 
 CREATE TABLE Labs (
   labID INT(11) PRIMARY KEY AUTO_INCREMENT, # reference lab table entries
+  title VARCHAR(50) NOT NULL,
   number INT(6) UNIQUE NOT NULL, # display lab numbers in order
   labDate VARCHAR(12) NOT NULL, # (MM/DD/YYYY) date of the lab assigned
   labTopics VARCHAR(150) NOT NULL, # topics covered in the lab
-  pdfLink INT(11) NOT NULL, # link to the lab pdf
+  pdfLink INT(11), # link to the lab pdf
   additionalFiles BOOLEAN NOT NULL DEFAULT FALSE, # whether or not there are additional files associated with the lab
-  solutionLink INT(11) NOT NULL, # link to lab solution zip file / github repo
+  solutionLink INT(11), # link to lab solution zip file / github repo
   FOREIGN KEY fk4(pdfLink) REFERENCES Links(linkID),
   FOREIGN KEY fk5(solutionLink) REFERENCES Links(linkID)
 );
 
 CREATE TABLE PreviousExams (
   examID INT(11) PRIMARY KEY AUTO_INCREMENT, # reference exam table entries
-  examDate VARCHAR(12) NOT NULL, # (MM/DD/YYYY) date of the exam
+  examDate VARCHAR(30) NOT NULL, # such as "Spring 2018" or "Fall 2017"
   type VARCHAR(20) NOT NULL, # (written/programming/other) type of the exam
-  pdfLink INT(11) NOT NULL, # link to the exam pdf file
+  pdfLink INT(11), # link to the exam pdf file
   solutions BOOLEAN NOT NULL, # whether or not there are solution links
   FOREIGN KEY fk6(pdfLink) REFERENCES Links(linkID)
 );
@@ -80,12 +81,12 @@ CREATE TABLE Calendar (
   title VARCHAR(30) NOT NULL, # title of the event
   date VARCHAR(12) NOT NULL, # date of the event
   startTime VARCHAR(8) NOT NULL, # (HH:MM) start time of the event
-  endTime VARCHAR(8) NOT NULL, # (HH:MM) end time of the event
+  endTime VARCHAR(8) NOT NULL # (HH:MM) end time of the event
 );
 
 CREATE TABLE Attendance (
   attendanceID INT(11) PRIMARY KEY AUTO_INCREMENT, # reference entries in attendance table
-  uscID INT(11) NOT NULL, # associate an entry with a user
+  uscID BIGINT(11) NOT NULL, # associate an entry with a user
   date VARCHAR(12) NOT NULL, # (MM/DD/YYYY) date of the user checked into class
   lecturePeriod INT(11) NOT NULL, # lecture period the user checked in
   FOREIGN KEY fk12(uscID) REFERENCES Account(uscID)
